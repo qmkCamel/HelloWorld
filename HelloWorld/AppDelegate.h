9e6a8faac446317e4b1c8f0bdc81098f8715e06d
qmkCamel <qumeikai@iyunxiao.com>
2015-11-08 17:15:47 +0800

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Camel on 15/11/8.
+//  Copyright © 2015年 Camel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
